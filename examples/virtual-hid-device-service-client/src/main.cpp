b05637da4444aa780678b850260a40b1e917edf9
Aidan Gibson <tronicdude@gmail.com>
2022-11-13 12:07:53 -0800

    client2 yeeted

--- examples/virtual-hid-device-service-client/src/main.cpp
+++ examples/virtual-hid-device-service-client/src/main.cpp
@@ -19,13 +19,14 @@ int main(void) {
   pqrs::dispatcher::extra::initialize_shared_dispatcher();
 
   std::filesystem::path client_socket_file_path1("/tmp/karabiner_driverkit_virtual_hid_device_service_client1.sock");
-  std::filesystem::path client_socket_file_path2("/tmp/karabiner_driverkit_virtual_hid_device_service_client2.sock");
+  //  std::filesystem::path client_socket_file_path2("/tmp/karabiner_driverkit_virtual_hid_device_service_client2.sock");
 
   std::mutex client_mutex;
   auto client1 = std::make_unique<pqrs::karabiner::driverkit::virtual_hid_device_service::client>(client_socket_file_path1);
-  auto client2 = std::make_unique<pqrs::karabiner::driverkit::virtual_hid_device_service::client>(client_socket_file_path2);
+  //  auto client2 = std::make_unique<pqrs::karabiner::driverkit::virtual_hid_device_service::client>(client_socket_file_path2);
 
-  std::thread call_ready_thread([&client1, &client2, &client_mutex] {
+  //  std::thread call_ready_thread([&client1, &client2, &client_mutex] {
+  std::thread call_ready_thread([&client1, &client_mutex] {
     while (!exit_flag) {
       {
         std::lock_guard<std::mutex> lock(client_mutex);
@@ -37,12 +38,12 @@ int main(void) {
           client1->async_virtual_hid_pointing_ready();
         }
 
-        if (client2) {
-          client2->async_driver_loaded();
-          client2->async_driver_version_matched();
-          client2->async_virtual_hid_keyboard_ready();
-          client2->async_virtual_hid_pointing_ready();
-        }
+        //        if (client2) {
+        //          client2->async_driver_loaded();
+        //          client2->async_driver_version_matched();
+        //          client2->async_virtual_hid_keyboard_ready();
+        //          client2->async_virtual_hid_pointing_ready();
+        //        }
       }
 
       std::this_thread::sleep_for(std::chrono::milliseconds(500));
@@ -51,7 +52,7 @@ int main(void) {
 
   std::mutex keyboard_thread_mutex;
   std::unique_ptr<std::thread> keyboard_thread1;
-  std::unique_ptr<std::thread> keyboard_thread2;
+  //  std::unique_ptr<std::thread> keyboard_thread2;
 
   std::mutex pointing_thread_mutex;
   std::unique_ptr<std::thread> pointing_thread1;
@@ -112,6 +113,7 @@ int main(void) {
                 pqrs::karabiner::driverkit::virtual_hid_device_driver::hid_report::keyboard_input report;
                 report.modifiers.insert(pqrs::karabiner::driverkit::virtual_hid_device_driver::hid_report::modifier::left_shift);
                 report.keys.insert(type_safe::get(pqrs::hid::usage::keyboard_or_keypad::keyboard_e));
+                //                 report.keys.insert(type_safe::get(pqrs::hid::usage::apple_vendor_top_case::illumination_down));
                 client1->async_post_report(report);
               }
 
@@ -163,43 +165,44 @@ int main(void) {
   // client2
   //
 
-  client2->connected.connect([&client2] {
-    client2->async_virtual_hid_keyboard_initialize(pqrs::hid::country_code::us);
-  });
-  client2->virtual_hid_keyboard_ready_response.connect([&client2, &client_mutex, &keyboard_thread2, &keyboard_thread_mutex](auto&& ready) {
-    if (ready) {
-      std::lock_guard<std::mutex> lock(keyboard_thread_mutex);
-
-      if (!keyboard_thread2) {
-        keyboard_thread2 = std::make_unique<std::thread>([&client2, &client_mutex] {
-          std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-          {
-            std::lock_guard<std::mutex> lock(client_mutex);
-
-            if (client2) {
-              // key down (g)
-              {
-                pqrs::karabiner::driverkit::virtual_hid_device_driver::hid_report::keyboard_input report;
-                report.keys.insert(type_safe::get(pqrs::hid::usage::keyboard_or_keypad::keyboard_g));
-                client2->async_post_report(report);
-              }
-
-#if 0
-              // key up
-              {
-                pqrs::karabiner::driverkit::virtual_hid_device_driver::hid_report::keyboard_input report;
-                client2->async_post_report(report);
-              }
-#endif
-            }
-          }
-        });
-      }
-    }
-  });
-
-  client2->async_start();
+  //  client2->connected.connect([&client2] {
+  //    client2->async_virtual_hid_keyboard_initialize(pqrs::hid::country_code::us);
+  //  });
+  //  client2->virtual_hid_keyboard_ready_response.connect([&client2, &client_mutex, &keyboard_thread2, &keyboard_thread_mutex](auto&& ready) {
+  //    if (ready) {
+  //      std::lock_guard<std::mutex> lock(keyboard_thread_mutex);
+  //
+  //      if (!keyboard_thread2) {
+  //        keyboard_thread2 = std::make_unique<std::thread>([&client2, &client_mutex] {
+  //          std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  //
+  //          {
+  //            std::lock_guard<std::mutex> lock(client_mutex);
+  //
+  //            if (client2) {
+  //              // key down (g)
+  //              {
+  //                pqrs::karabiner::driverkit::virtual_hid_device_driver::hid_report::keyboard_input report;
+  //                report.keys.insert(type_safe::get(pqrs::hid::usage::keyboard_or_keypad::keyboard_g));
+  ////                 report.keys.insert(type_safe::get(pqrs::hid::usage::apple_vendor_top_case::illumination_down));
+  //                client2->async_post_report(report);
+  //              }
+  //
+  //#if 0
+  //              // key up
+  //              {
+  //                pqrs::karabiner::driverkit::virtual_hid_device_driver::hid_report::keyboard_input report;
+  //                client2->async_post_report(report);
+  //              }
+  //#endif
+  //            }
+  //          }
+  //        });
+  //      }
+  //    }
+  //  });
+  //
+  //  client2->async_start();
 
   //
   // Wait control-c
@@ -224,7 +227,7 @@ int main(void) {
   {
     std::lock_guard<std::mutex> lock(client_mutex);
     client1 = nullptr;
-    client2 = nullptr;
+    //    client2 = nullptr;
   }
 
   {
@@ -232,9 +235,9 @@ int main(void) {
     if (keyboard_thread1) {
       keyboard_thread1->join();
     }
-    if (keyboard_thread2) {
-      keyboard_thread2->join();
-    }
+    //    if (keyboard_thread2) {
+    //      keyboard_thread2->join();
+    //    }
   }
 
   {
